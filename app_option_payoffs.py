878aecd37b8be382939ac49236ece191766c03a4
jeremy.tang <jeremy.cy.tang@gmail.com>
2019-02-28 14:39:07 +1100

    revised version

--- app_option_payoffs.py
+++ app_option_payoffs.py
@@ -21,14 +21,19 @@ df = pd.read_sql(sql, conn)
 portfolios = df['Portfolio Code'].unique().tolist()
 portfolios.sort()
 prt_options = [dict(label=str(prt), value=str(prt)) for prt in portfolios]
+fields = ['Security', 'Security Type', 'Unit Holding', 'Lot Size', 'Expiry Date', 'Market Price', 'Excercise Price',
+          'Average Cost', 'Total Cost']
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
 
-    # Hidden div inside the app that stores the filtered dataframe to use in other callbacks
-    html.Div(id='filtered_df', style={'display': 'none'}),
+    # Hidden div inside the app for use with arbitrary callback
+    html.Div(id='hidden', style={'display': 'none'}),
+
+    # store component to store the filtered dataframe
+    dcc.Store(id='filtered_df'),
 
     # App Heading
     html.Div([
@@ -46,13 +51,9 @@ app.layout = html.Div([
         dcc.Dropdown(
             id='prt-dropdown',
             options=prt_options
-            # value='IMDVFP'
+
         )
 
-        # html.Label('Select Issuer'),
-        # dcc.Dropdown(
-        #     id='issuer-dropdown'
-        # )
     ], style={'display': 'table-cell', 'width': '25%'}),
 
     # Issuer drop down box
@@ -66,7 +67,10 @@ app.layout = html.Div([
     # dash table
     html.Div([
         dash_table.DataTable(
-            id='sec_table'
+            id='sec_table',
+            columns=[{"name": i, "id": i} for i in fields],
+            editable=True,
+            row_deletable=True
         )
     ], style={'width': '50%'}),
 
@@ -93,11 +97,12 @@ def update_issuer_dropdown(prt):
 
 # callback function to return a dataframe that contains issuer data for the selected portfolio and issuer code
 @app.callback(
-    dash.dependencies.Output('filtered_df', 'children'),
+    dash.dependencies.Output('filtered_df', 'data'),
     [dash.dependencies.Input('prt-dropdown', 'value'),
      dash.dependencies.Input('issuer-dropdown', 'value')]
 )
 def clean_data(prt, issuer):
+    print('storing issuer data in dataframe')
     if prt is not None and issuer is not None:
         sec_types = ['OS', 'CO', 'PO']
         df_filter = df[(df['Portfolio Code'] == prt) & (df['Security Type'].isin(sec_types)) &
@@ -105,96 +110,94 @@ def clean_data(prt, issuer):
         df_filter = df_filter[((df_filter['Security Type'] == 'OS') & (df_filter['Security'].str.len() == 3)) |
             (df_filter['Security Type'].isin(['CO', 'PO']))].copy()
         df_filter['Average Cost'] = df_filter['Total Cost'] / (df_filter['Unit Holding'] * df_filter['Lot Size'])
-        fields = ['Security', 'Security Type', 'Unit Holding', 'Lot Size', 'Expiry Date', 'Market Price', 'Excercise Price',
-                  'Average Cost', 'Total Cost']
-        df_filter = df_filter.loc[:, fields]
-        return df_filter.to_json(date_format='iso', orient='split')
+        df_filter = df_filter.loc[:, fields].copy()
+        return df_filter.to_dict('rows')
 
     else:
-        return '{}'
-
-
-# callback function to display columns on table
-@app.callback(
-    dash.dependencies.Output('sec_table', 'columns'),
-    [dash.dependencies.Input('filtered_df', 'children')]
-)
-def display_columns(json_data):
-    df_filter = pd.read_json(json_data, orient='split')
-    return [{"name": i, "id": i} for i in df_filter.columns]
+        return [{}]
 
 # callback function to display row data on table
 @app.callback(
     dash.dependencies.Output('sec_table', 'data'),
-    [dash.dependencies.Input('filtered_df', 'children')]
+    [dash.dependencies.Input('filtered_df', 'data')]
 )
-def display_rows(json_data):
-    df_filter = pd.read_json(json_data, orient='split')
-    return df_filter.to_dict("rows")
+def display_rows(data):
+    if data is None:
+        raise dash.exceptions.PreventUpdate
+
+    return data
+
+# arbitrary callback so editable table will update
+@app.callback(
+    dash.dependencies.Output('hidden', 'children'),
+    [dash.dependencies.Input('sec_table', 'data_timestamp')]
+)
+def dummy(d):
+    raise dash.exceptions.PreventUpdate
 
 # callback function to update payoff chart
 @app.callback(
     dash.dependencies.Output('payoff_graph', 'figure'),
-    [dash.dependencies.Input('filtered_df', 'children')]
+    [dash.dependencies.Input('filtered_df', 'data')]
 )
-def update_chart(json_data):
-    if json_data == '{}':
-        figure = dict()
+def update_chart(data):
+    print('chart update')
+    if data == [{}]:
+        raise dash.exceptions.PreventUpdate
 
-    else:
-        df_filter = pd.read_json(json_data, orient='split')
-        issuer = df_filter[df_filter['Security Type'] == 'OS']['Security'].values[0]
-        min_price = 0
-        max_price = max(df_filter['Market Price'].max(), df_filter['Excercise Price'].max()) * 1.2
-        prices = np.arange(min_price, max_price, 0.01)
-        df_payoff = pd.DataFrame(index=prices)
+    df_filter = pd.DataFrame(data=data)
+    issuer = df_filter[df_filter['Security Type'] == 'OS']['Security'].values[0]
+    min_price = 0
+    max_price = max(df_filter['Market Price'].max(), df_filter['Excercise Price'].max()) * 2
+    prices = np.arange(min_price, max_price, 0.01)
+    df_payoff = pd.DataFrame(index=prices)
 
-        # calculate payoffs and store in a dataframe
-        sec_count = 0
-        for idx, row in df_filter.iterrows():
-            if row['Security Type'] == 'OS':
-                df_payoff[sec_count] = row['Unit Holding'] * df_payoff.index - row['Total Cost']
+    # calculate payoffs and store in a dataframe
+    sec_count = 0
+    for idx, row in df_filter.iterrows():
+        if row['Security Type'] == 'OS':
+            df_payoff[sec_count] = row['Unit Holding'] * df_payoff.index - row['Total Cost']
 
-            elif row['Security Type'] == 'CO':
-                payoff = df_payoff.index - row['Excercise Price']
-                df_payoff.loc[payoff >= 0, sec_count] = row['Unit Holding'] * row['Lot Size'] * payoff[payoff >= 0] - \
-                    row['Total Cost']
-                df_payoff.loc[payoff < 0, sec_count] = 0 - row['Total Cost']
+        elif row['Security Type'] == 'CO':
+            payoff = df_payoff.index - row['Excercise Price']
+            df_payoff.loc[payoff >= 0, sec_count] = row['Unit Holding'] * row['Lot Size'] * payoff[payoff >= 0] - \
+                row['Total Cost']
+            df_payoff.loc[payoff < 0, sec_count] = 0 - row['Total Cost']
 
-            elif row['Security Type'] == 'PO':
-                payoff = row['Excercise Price'] - df_payoff.index
-                df_payoff.loc[payoff >= 0, sec_count] = row['Unit Holding'] * row['Lot Size'] * payoff[payoff >= 0] - \
-                    row['Total Cost']
-                df_payoff.loc[payoff < 0, sec_count] = 0 - row['Total Cost']
+        elif row['Security Type'] == 'PO':
+            payoff = row['Excercise Price'] - df_payoff.index
+            df_payoff.loc[payoff >= 0, sec_count] = row['Unit Holding'] * row['Lot Size'] * payoff[payoff >= 0] - \
+                row['Total Cost']
+            df_payoff.loc[payoff < 0, sec_count] = 0 - row['Total Cost']
 
-            sec_count += 1
+        sec_count += 1
 
-        df_payoff.loc[:, 'Total'] = df_payoff.sum(axis=1)
+    df_payoff.loc[:, 'Total'] = df_payoff.sum(axis=1)
 
-        # option payoff chart
-        scatter = [go.Scatter(
-            x=df_payoff.index,
-            y=df_payoff['Total'],
-            mode='lines',
-            name=issuer,
+    # option payoff chart
+    scatter = [go.Scatter(
+        x=df_payoff.index,
+        y=df_payoff['Total'],
+        mode='lines',
+        name=issuer,
+        showlegend=False
+    )]
+
+    strikes = df_filter.loc[df_filter['Excercise Price'] > 0, 'Excercise Price']
+    for strike in strikes:
+        scatter.append(go.Scatter(
+            x=df_payoff.loc[df_payoff.index == strike, :].index,
+            y=df_payoff.loc[df_payoff.index == strike, 'Total'],
+            mode='markers',
             showlegend=False
-        )]
+        ))
 
-        strikes = df_filter.loc[df['Excercise Price'] > 0, 'Excercise Price']
-        for strike in strikes:
-            scatter.append(go.Scatter(
-                x=df_payoff.loc[df_payoff.index == strike, :].index,
-                y=df_payoff.loc[df_payoff.index == strike, 'Total'],
-                mode='markers',
-                showlegend=False
-            ))
+    layout = dict(title='Payoff',
+                  xaxis=dict(title='Stock Price'),
+                  yaxis=dict(title='P&L ($)')
+                  )
 
-        layout = dict(title='Payoff',
-                      xaxis=dict(title='Stock Price'),
-                      yaxis=dict(title='P&L ($)')
-                      )
-
-        figure = dict(data=scatter, layout=layout)
+    figure = dict(data=scatter, layout=layout)
 
     return figure
 
